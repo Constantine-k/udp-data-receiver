6af8bf868cad7f31146ada9b951ff87960924a82
Konstantin Konstantinov <kostiantyn.konstantinov@cloudmade.com>
2019-10-07 02:22:20 +0300

    Initial Commit

---
+++ ServerDataReceiver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ServerDataReceiver
+//
+//  Created by Konstantin Konstantinov on 10/7/19.
+//  Copyright © 2019 Konstantin Konstantinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
